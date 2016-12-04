c0b1bfd483f58edcf7826693f03776cf759ea065
domenic99 <alexander95giz@gmail.com>
2016-12-04 15:52:15 +0200

    Username added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
   has_many :articles
 end

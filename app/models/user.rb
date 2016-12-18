c985812347343b17b2c3f6c71de32e5bc024c8fc
domenic99 <alexander95giz@gmail.com>
2016-12-18 17:29:54 +0200

    bucket to paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/kit.jpg"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
+    default_url: "/images/kit.jpg",
+    bucket: '******'
   validates :avatar, attachment_presence: true
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
